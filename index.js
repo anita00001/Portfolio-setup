70f0780581fd3550fc28c2e0739f901b0b92ef50
zunairkhan811 <zunairkhan811@gmail.com>
2023-01-31 15:08:30 +0500

    Add javascript code and css changes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-//empty file
+/* eslint-disable no-unused-vars */
+const menuiconId = document.querySelector('#menu-icon');
+const logoId = document.querySelector('#logo');
+const openmenuId = document.querySelector('#open_menu');
+const portfolioicon = document.querySelector('#portfolio-icon');
+const abouticon = document.querySelector('#about-icon');
+const contacticon = document.querySelector('#contact-icon');
+
+// eslint-disable-next-line no-unused-vars, func-names
+const changeIcon = function (icon) {
+  icon.classList.toggle('fa-times');
+  icon.classList.toggle('change-menu');
+  icon.classList.toggle('change-menu-color');
+  logoId.classList.toggle('change-logo');
+  openmenuId.classList.toggle('change-open_menu');
+};
